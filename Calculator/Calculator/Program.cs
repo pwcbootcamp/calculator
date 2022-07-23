94203273110388441f6cbc76c102c6d556e288a1
PhantomOz <faniogor@gmail.com>
2022-07-23 22:14:25 +0100

    My C# Calculator Assignment for Group 6

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("\n      Welcome To CalCulatorPedia\n\n A calculator that performs simple arithemetic calculations like:\n\n   1. Multiplication(*)\n" +
+    "   2. Division(/)\n   3. Addition(+)\n   4. Subtraction(-).\n\n");
+Console.WriteLine("Please Enter Your First Number");
+var firstNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Please Enter The Operator");
+var Operator = Console.ReadLine();
+Console.WriteLine("Please Enter Your Second Number");
+var secondNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine(firstNumber);
+
+switch (Operator)
+{
+    case "x":
+        Console.WriteLine(firstNumber * secondNumber);
+        break;
+    case "*":
+        Console.WriteLine(firstNumber * secondNumber);
+        break;
+    case "-":
+        Console.WriteLine(firstNumber - secondNumber);
+        break;
+    case "+":
+        Console.WriteLine(firstNumber + secondNumber);
+        break;
+    case "/":
+        Console.WriteLine(firstNumber / secondNumber);
+        break;
+    default:
+        Console.WriteLine("No Operator passed");
+        break;
+}
