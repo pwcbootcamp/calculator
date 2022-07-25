19e979a3f164c8c68191f0282999b7ba1e6f3ca2
oyagbile oluwaseun <oyagbileoluwaseun@gmail.com>
2022-07-25 21:49:56 +0100

    Calculator Assignment

---
+++ CalculatorOyagbile/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace CalculatorOyagbile
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.BtnRes = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.Btn0 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.BtnSub = new System.Windows.Forms.Button();
+            this.Btn6 = new System.Windows.Forms.Button();
+            this.Btn5 = new System.Windows.Forms.Button();
+            this.Btn4 = new System.Windows.Forms.Button();
+            this.BtnMul = new System.Windows.Forms.Button();
+            this.Btn9 = new System.Windows.Forms.Button();
+            this.Btn8 = new System.Windows.Forms.Button();
+            this.BtnDiv = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.BtnCopy = new System.Windows.Forms.Button();
+            this.Btn7 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.txtDisplay, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(297, 339);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtDisplay.Location = new System.Drawing.Point(3, 3);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.Size = new System.Drawing.Size(291, 61);
+            this.txtDisplay.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.BtnRes, 3, 4);
+            this.tableLayoutPanel2.Controls.Add(this.button19, 2, 4);
+            this.tableLayoutPanel2.Controls.Add(this.Btn0, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.button17, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.BtnAdd, 3, 3);
+            this.tableLayoutPanel2.Controls.Add(this.Btn3, 2, 3);
+            this.tableLayoutPanel2.Controls.Add(this.Btn2, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.Btn1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.BtnSub, 3, 2);
+            this.tableLayoutPanel2.Controls.Add(this.Btn6, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.Btn5, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.Btn4, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.BtnMul, 3, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Btn9, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Btn8, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.BtnDiv, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.BtnClear, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.BtnReset, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.BtnCopy, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Btn7, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 70);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(291, 266);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // BtnRes
+            // 
+            this.BtnRes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnRes.Location = new System.Drawing.Point(219, 215);
+            this.BtnRes.Name = "BtnRes";
+            this.BtnRes.Size = new System.Drawing.Size(69, 48);
+            this.BtnRes.TabIndex = 0;
+            this.BtnRes.Text = "=";
+            this.BtnRes.UseVisualStyleBackColor = true;
+            this.BtnRes.Click += new System.EventHandler(this.BtnRes_Click);
+            // 
+            // button19
+            // 
+            this.button19.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button19.Location = new System.Drawing.Point(147, 215);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(66, 48);
+            this.button19.TabIndex = 0;
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // Btn0
+            // 
+            this.Btn0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn0.Location = new System.Drawing.Point(75, 215);
+            this.Btn0.Name = "Btn0";
+            this.Btn0.Size = new System.Drawing.Size(66, 48);
+            this.Btn0.TabIndex = 0;
+            this.Btn0.Text = "0";
+            this.Btn0.UseVisualStyleBackColor = true;
+            this.Btn0.Click += new System.EventHandler(this.Btn0_Click);
+            // 
+            // button17
+            // 
+            this.button17.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button17.Location = new System.Drawing.Point(3, 215);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(66, 48);
+            this.button17.TabIndex = 0;
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnAdd.Location = new System.Drawing.Point(219, 162);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(69, 47);
+            this.BtnAdd.TabIndex = 0;
+            this.BtnAdd.Text = "+";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // Btn3
+            // 
+            this.Btn3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn3.Location = new System.Drawing.Point(147, 162);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(66, 47);
+            this.Btn3.TabIndex = 0;
+            this.Btn3.Text = "3";
+            this.Btn3.UseVisualStyleBackColor = true;
+            this.Btn3.Click += new System.EventHandler(this.Btn3_Click);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn2.Location = new System.Drawing.Point(75, 162);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(66, 47);
+            this.Btn2.TabIndex = 0;
+            this.Btn2.Text = "2";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // Btn1
+            // 
+            this.Btn1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn1.Location = new System.Drawing.Point(3, 162);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(66, 47);
+            this.Btn1.TabIndex = 0;
+            this.Btn1.Text = "1";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.BtnNum_Click);
+            // 
+            // BtnSub
+            // 
+            this.BtnSub.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnSub.Location = new System.Drawing.Point(219, 109);
+            this.BtnSub.Name = "BtnSub";
+            this.BtnSub.Size = new System.Drawing.Size(69, 47);
+            this.BtnSub.TabIndex = 0;
+            this.BtnSub.Text = "-";
+            this.BtnSub.UseVisualStyleBackColor = true;
+            this.BtnSub.Click += new System.EventHandler(this.BtnSub_Click);
+            // 
+            // Btn6
+            // 
+            this.Btn6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn6.Location = new System.Drawing.Point(147, 109);
+            this.Btn6.Name = "Btn6";
+            this.Btn6.Size = new System.Drawing.Size(66, 47);
+            this.Btn6.TabIndex = 0;
+            this.Btn6.Text = "6";
+            this.Btn6.UseVisualStyleBackColor = true;
+            this.Btn6.Click += new System.EventHandler(this.Btn6_Click);
+            // 
+            // Btn5
+            // 
+            this.Btn5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn5.Location = new System.Drawing.Point(75, 109);
+            this.Btn5.Name = "Btn5";
+            this.Btn5.Size = new System.Drawing.Size(66, 47);
+            this.Btn5.TabIndex = 0;
+            this.Btn5.Text = "5";
+            this.Btn5.UseVisualStyleBackColor = true;
+            this.Btn5.Click += new System.EventHandler(this.Btn5_Click);
+            // 
+            // Btn4
+            // 
+            this.Btn4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn4.Location = new System.Drawing.Point(3, 109);
+            this.Btn4.Name = "Btn4";
+            this.Btn4.Size = new System.Drawing.Size(66, 47);
+            this.Btn4.TabIndex = 0;
+            this.Btn4.Text = "4";
+            this.Btn4.UseVisualStyleBackColor = true;
+            this.Btn4.Click += new System.EventHandler(this.Btn4_Click);
+            // 
+            // BtnMul
+            // 
+            this.BtnMul.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnMul.Location = new System.Drawing.Point(219, 56);
+            this.BtnMul.Name = "BtnMul";
+            this.BtnMul.Size = new System.Drawing.Size(69, 47);
+            this.BtnMul.TabIndex = 0;
+            this.BtnMul.Text = "*";
+            this.BtnMul.UseVisualStyleBackColor = true;
+            this.BtnMul.Click += new System.EventHandler(this.BtnMul_Click);
+            // 
+            // Btn9
+            // 
+            this.Btn9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn9.Location = new System.Drawing.Point(147, 56);
+            this.Btn9.Name = "Btn9";
+            this.Btn9.Size = new System.Drawing.Size(66, 47);
+            this.Btn9.TabIndex = 0;
+            this.Btn9.Text = "9";
+            this.Btn9.UseVisualStyleBackColor = true;
+            this.Btn9.Click += new System.EventHandler(this.Btn9_Click);
+            // 
+            // Btn8
+            // 
+            this.Btn8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn8.Location = new System.Drawing.Point(75, 56);
+            this.Btn8.Name = "Btn8";
+            this.Btn8.Size = new System.Drawing.Size(66, 47);
+            this.Btn8.TabIndex = 0;
+            this.Btn8.Text = "8";
+            this.Btn8.UseVisualStyleBackColor = true;
+            this.Btn8.Click += new System.EventHandler(this.Btn8_Click);
+            // 
+            // BtnDiv
+            // 
+            this.BtnDiv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnDiv.Location = new System.Drawing.Point(219, 3);
+            this.BtnDiv.Name = "BtnDiv";
+            this.BtnDiv.Size = new System.Drawing.Size(69, 47);
+            this.BtnDiv.TabIndex = 0;
+            this.BtnDiv.Text = "/";
+            this.BtnDiv.UseVisualStyleBackColor = true;
+            this.BtnDiv.Click += new System.EventHandler(this.BtnDiv_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnClear.Location = new System.Drawing.Point(147, 3);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(66, 47);
+            this.BtnClear.TabIndex = 0;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnReset.Location = new System.Drawing.Point(75, 3);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(66, 47);
+            this.BtnReset.TabIndex = 0;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // BtnCopy
+            // 
+            this.BtnCopy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnCopy.Location = new System.Drawing.Point(3, 3);
+            this.BtnCopy.Name = "BtnCopy";
+            this.BtnCopy.Size = new System.Drawing.Size(66, 47);
+            this.BtnCopy.TabIndex = 0;
+            this.BtnCopy.Text = "Copy";
+            this.BtnCopy.UseVisualStyleBackColor = true;
+            this.BtnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            // 
+            // Btn7
+            // 
+            this.Btn7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn7.Location = new System.Drawing.Point(3, 56);
+            this.Btn7.Name = "Btn7";
+            this.Btn7.Size = new System.Drawing.Size(66, 47);
+            this.Btn7.TabIndex = 0;
+            this.Btn7.Text = "7";
+            this.Btn7.UseVisualStyleBackColor = true;
+            this.Btn7.Click += new System.EventHandler(this.Btn7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 354);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button BtnRes;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button Btn0;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button BtnSub;
+        private System.Windows.Forms.Button Btn6;
+        private System.Windows.Forms.Button Btn5;
+        private System.Windows.Forms.Button Btn4;
+        private System.Windows.Forms.Button BtnMul;
+        private System.Windows.Forms.Button Btn9;
+        private System.Windows.Forms.Button Btn8;
+        private System.Windows.Forms.Button BtnDiv;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Button BtnCopy;
+        private System.Windows.Forms.Button Btn7;
+    }
+}
+
---
+++ CalculatorOyagbile/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorOyagbile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtDisplay.Text);
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            previousOperation = Operation.None;
+            txtDisplay.Clear();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            if(txtDisplay.Text.Length > 0)
+            {
+                double d;
+                if (double.TryParse(txtDisplay.Text[txtDisplay.Text.Length - 1].ToString(), out d))
+                {
+                    previousOperation = Operation.None;
+                }
+                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDiv_Click(object sender, EventArgs e)
+        {
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+
+            previousOperation = Operation.Div;    
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void BtnNum_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn1 as Button).Text;
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn2 as Button).Text;
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn3 as Button).Text;
+        }
+
+        private void Btn4_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn4 as Button).Text;
+        }
+
+        private void Btn5_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn5 as Button).Text;
+        }
+
+        private void Btn6_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn6 as Button).Text;
+        }
+
+        private void Btn7_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn7 as Button).Text;
+        }
+
+        private void Btn8_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn8 as Button).Text;
+        }
+
+        private void Btn9_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn9 as Button).Text;
+        }
+
+        private void Btn0_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += (Btn0 as Button).Text;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+
+            previousOperation = Operation.Add;     
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void PerformCalculation(Operation previousOperation)
+        {
+            List<double> lstNums = new List<double>();
+            switch (previousOperation)
+            {
+                case Operation.Add:
+                    lstNums = txtDisplay.Text.Split('+').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] + lstNums[1]).ToString();
+                    break;
+                case Operation.Sub:
+                    lstNums = txtDisplay.Text.Split('-').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] - lstNums[1]).ToString();
+                    break;
+                case Operation.Mul:
+                    lstNums = txtDisplay.Text.Split('*').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] * lstNums[1]).ToString();
+                    break;
+                case Operation.Div:
+                    try
+                    {
+                        lstNums = txtDisplay.Text.Split('/').Select(double.Parse).ToList();
+                        txtDisplay.Text = (lstNums[0] / lstNums[1]).ToString();
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        txtDisplay.Text = "EEEEEEE";
+                    }
+                   
+                    break;
+                case Operation.None:
+                    break;
+            }
+        }
+
+        private void BtnMul_Click(object sender, EventArgs e)
+        {
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+
+            previousOperation = Operation.Mul;     
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void BtnSub_Click(object sender, EventArgs e)
+        {
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+            
+            previousOperation = Operation.Sub;                
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        enum Operation
+        {
+            Add,
+            Sub,
+            Mul,
+            Div,
+            None
+        }
+
+        static Operation previousOperation = Operation.None;
+
+        private void BtnRes_Click(object sender, EventArgs e)
+        {
+            if (previousOperation == Operation.None)
+                return;
+            else
+                PerformCalculation(previousOperation);
+        }
+    }
+}
