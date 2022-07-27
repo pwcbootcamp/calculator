37ab3c16f8888e4854a53f8be2ed0c570ac0139a
Fisayor <oluwafisayoowoyele@gmail.com>
2022-07-27 08:43:30 -0700

    first commit

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double num1, num2, result = 0;
+
+        Console.Write("Enter first number:");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number:");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter an operator (+, -, *, /): ");
+        char operand = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        switch (operand)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("You have entered wrong operator");
+                break;
+        }
+
+        Console.WriteLine(num1 + " " + operand + " " + num2 + ": " + result);
+        Console.ReadLine();
+    }
+}
