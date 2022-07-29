8159e10337f7d276d0f20ab0fcb4174a2d44c4a5
ayo <hakeemanibaba@yahoo.com>
2022-07-29 13:55:27 +0100

    first commit

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input first number");
+var firstNum = Console.ReadLine();
+Console.WriteLine("Enter operator (+,-,/,*)");
+var operatorType = Console.ReadLine();
+Console.WriteLine("Input second Number");
+var secondNum = Console.ReadLine();
+double resultFirstNum;
+double resultSecondNum;
+double result;
+if (double.TryParse(firstNum, out resultFirstNum) && double.TryParse(secondNum, out resultSecondNum))
+{
+    switch (operatorType)
+    {
+        case "+":
+            result = resultFirstNum + resultSecondNum;
+            Console.WriteLine(result);
+            break;
+        case "-":
+            result = resultFirstNum - resultSecondNum;
+            Console.WriteLine(result);
+            break;
+        case "*":
+            result = resultFirstNum * resultSecondNum;
+            Console.WriteLine(result);
+            break;
+        case "/":
+            result = resultFirstNum / resultSecondNum;
+            Console.WriteLine(result);
+            break;
+        default:
+            Console.WriteLine("Invalid operator please try again!");
+            break;
+    };
+}
+else
+{
+    Console.WriteLine("Failure: You have added a wrong input!");
+}
 
